3dd6c56f9677178971e4f9e2fd53b6c33d670428
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2023-09-25 23:08:07 +0900

    Initial commit

---
+++ test/cdk-microservices-rollingupdate-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMicroservicesRollingupdateTemplate from '../lib/cdk-microservices-rollingupdate-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-microservices-rollingupdate-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMicroservicesRollingupdateTemplate.CdkMicroservicesRollingupdateTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
